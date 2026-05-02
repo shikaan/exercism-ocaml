d28174992f8b05d990e41bae12876d9a09097694
Manuel Spagnolo <manuel.spagnolo@proton.me>
2026-05-02 23:03:26 +0200

    rna

--- rna-transcription/rna_transcription.ml
+++ rna-transcription/rna_transcription.ml
@@ -1,5 +1,4 @@
 type dna = [ `A | `C | `G | `T ]
 type rna = [ `A | `C | `G | `U ]
 
-let to_rna _ =
-    failwith "'to_rna' is missing"
+let to_rna = List.map (function `A -> `U | `G -> `C | `T -> `A | `C -> `G)
