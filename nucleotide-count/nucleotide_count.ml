9d6d4692244fe960ed4591e494c43b5f47277ed3
Manuel Spagnolo <manuel.spagnolo@proton.me>
2026-05-02 22:10:57 +0200

    nucleotide, one pass

--- nucleotide-count/nucleotide_count.ml
+++ nucleotide-count/nucleotide_count.ml
@@ -13,10 +13,19 @@ let count_nucleotide s c =
         if isvalid c then Ok (String.count ~f:(fun e -> Char.equal e c) s)
         else Error c)
 
+(* let count_nucleotides s = *)
+(*   match String.find s ~f:isinvalid with *)
+(*   | Some invalid -> Result.Error invalid *)
+(*   | None -> *)
+(*       Result.Ok( *)
+(*         String.fold s ~init:empty ~f:(fun acc c -> *)
+(*             Map.update acc c ~f:(function None -> 1 | Some n -> n + 1))) *)
+
 let count_nucleotides s =
-  match String.find s ~f:isinvalid with
-  | Some invalid -> Result.Error invalid
-  | None ->
-      Result.Ok(
-        String.fold s ~init:empty ~f:(fun acc c ->
-            Map.update acc c ~f:(function None -> 1 | Some n -> n + 1)))
+  Result.(
+    String.fold s ~init:(Ok empty) ~f:(fun acc c -> match acc with
+    | Error _ -> acc
+    | Ok acc ->
+        if isinvalid c then Error c
+        else Ok (Map.update acc c ~f:(function None -> 1 | Some n -> n + 1)))
+  )
