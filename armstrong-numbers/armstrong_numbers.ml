63db96bef1865836970a06f285535be2df96c410
Manuel Spagnolo <manuel.spagnolo@proton.me>
2026-05-02 14:47:38 +0200

    armstrong

--- armstrong-numbers/armstrong_numbers.ml
+++ armstrong-numbers/armstrong_numbers.ml
@@ -1,1 +1,14 @@
-let validate: candidate = failwith = "'validate' is not implemented"
+  let rec pow base exp =
+    if exp = 0 then 1
+    else base * pow base (exp - 1)
+
+let rec armstrong n e sum =
+  match n with
+    | 0 -> sum
+    | _ -> 
+        let sum = sum + pow (Int.rem n 10) e in 
+        armstrong (n/10) e sum;;
+
+let validate n =
+  let exp = String.length (Int.to_string n) in
+  n = armstrong n exp 0 
