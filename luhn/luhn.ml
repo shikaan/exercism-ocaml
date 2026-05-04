0232b3dbfad78d7077f47662aef6a7366d05027e
Manuel Spagnolo <manuel.spagnolo@proton.me>
2026-05-04 09:12:27 +0200

    luhn

--- luhn/luhn.ml
+++ luhn/luhn.ml
@@ -1,17 +1,22 @@
-let digit n =
-  let d = n * 2 in
-  Result.(
-    match d with
-    | d when d < 0 -> Error "must be positive"
-    | d when d > 9 -> Ok (d - 9)
-    | d -> Ok d)
+open Base;;
 
-let parsedigit c = Char.code c - Char.code '0'
+let even x = Int.rem x 2 = 0
+
+let hash idx n =
+  if even idx then n
+  else
+    let d = n * 2 in
+    match d with d when d > 9 -> d - 9 | d -> d
+
+let digit c = match Char.get_digit c with Some n -> n | None -> 0
+
+let validstring s = String.length s > 1 && String.for_all s ~f:Char.is_digit 
 
 let valid s =
-  let digits = List.(String.to_seq s |> of_seq |> rev |> map parsedigit) in
-  let hash = List.(digits
-    |> mapi (fun i x -> Result.get_ok (digit(if i mod 2 = 0 then x else x * 2)))
-    |> fold_left (+) 0) in
-  hash mod 10 = 0
-
+  List.(
+    let cl = String.filter s ~f:(fun c -> not (Char.is_whitespace c)) in
+    if validstring cl then
+      let dits = map (String.to_list cl |> rev) ~f:digit in
+      let hashed = foldi dits ~f:(fun i acc e -> acc + hash i e) ~init:0 in
+      Int.rem hashed 10 = 0
+    else false)
